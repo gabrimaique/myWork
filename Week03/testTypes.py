0aefb3b7f91f24687c89a5a3a05c8972e78dad49
gabrimaique <tirolezz@hotmail.com>
2022-02-03 11:20:14 +0000

    myWork

---
+++ Week03/testTypes.py
@@ -0,0 +1,7 @@
+i = 3
+fl = 3.5
+isa = True
+memo = 'how now Brown Cow'
+lots = []
+print('variable {} is of type: {} and value: {}' .format('i', type(i), i))
+print('variable {} is of type: {} and value: {}' .format('fl', type(fl), fl))
