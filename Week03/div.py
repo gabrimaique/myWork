1f7c2dd40fa7f768d1a06887db11624394e34a1a
gabrimaique <tirolezz@hotmail.com>
2022-02-07 10:43:34 +0000

    Week03

---
+++ Week03/div.py
@@ -0,0 +1,6 @@
+#program that reads in two numbers and outputs the integer answer and remainder
+x = int(input('enter first number:'))
+y = int(input('enter the number you want to divide by:'))
+answer = int(x//y) #//gives the int division 
+remainder = x%y # % gives the remainder
+print('{} divided by {} is {} with remainder {}' .format(x, y, answer, remainder))
---
+++ Week03/randomGenerator.py
@@ -0,0 +1,3 @@
+import random
+number = random.randint(1,10)
+print('here is a random number {}'.format(number))
